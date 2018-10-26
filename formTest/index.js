9b3804e377de78f83316a2091159de8ccc8dd3c6
Alberto Pacheco <albertovazpacheco@hotmail.com>
2018-10-26 18:32:57 +0200

    structure done style missing

---
+++ formTest/index.js
@@ -0,0 +1,18 @@
+const infosButton = document.querySelector(".infos-nav");
+const paymentButton = document.querySelector(".payment-nav");
+const infosScreen = document.querySelector(".personal-info");
+const paymentScreen = document.querySelector(".payment-slide");
+
+infosButton.addEventListener("click", function() {
+  infosScreen.classList.remove("hidden");
+  paymentScreen.classList.add("hidden");
+  infosButton.classList.add("selected");
+  paymentButton.classList.remove("selected");
+});
+
+paymentButton.addEventListener("click", function() {
+  infosScreen.classList.add("hidden");
+  paymentScreen.classList.remove("hidden");
+  infosButton.classList.remove("selected");
+  paymentButton.classList.add("selected");
+});
