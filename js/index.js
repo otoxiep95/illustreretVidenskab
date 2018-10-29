a529ffdc73af2c9ba385389310f200216018722a
Alberto Pacheco <albertovazpacheco@hotmail.com>
2018-10-29 12:30:09 +0100

    validation done

--- js/index.js
+++ js/index.js
@@ -17,7 +17,6 @@ paymentButton.addEventListener("click", function() {
   infosButton.classList.remove("selected");
   paymentButton.classList.add("selected");
   body.style.backgroundColor = "lightblue";
-
 });
 
 const backArrow = document.querySelector(".arrow");
@@ -28,7 +27,7 @@ function GoBack() {
 
 backArrow.addEventListener("click", function() {
   GoBack();
-})
+});
 
 const body = document.querySelector("body");
 
@@ -38,6 +37,43 @@ function checkForm() {
   console.log("checkForm called");
   let validity = form.checkValidity();
   console.log("Validity", validity);
+
+  console.log(document.querySelector("#phonenr").checkValidity());
+  if (
+    document.querySelector("#phonenr").checkValidity() &&
+    document.querySelector("#phonenr").value !== ""
+  ) {
+    document
+      .querySelector("#phonenr")
+      .nextElementSibling.nextElementSibling.classList.remove("hidden");
+  } else if (document.querySelector("#phonenr").value === "") {
+    document
+      .querySelector("#phonenr")
+      .nextElementSibling.nextElementSibling.classList.add("hidden");
+  } else {
+    document
+      .querySelector("#phonenr")
+      .nextElementSibling.nextElementSibling.classList.add("hidden");
+  }
+
+  if (
+    document.querySelector("#confirmemail").value !=
+    document.querySelector("#email").value
+  ) {
+    //console.log(document.querySelector("#confirmemail + .validated"));
+    document
+      .querySelector("#confirmemail")
+      .nextElementSibling.nextElementSibling.classList.add("hidden");
+    document
+      .querySelector("#confirmemail")
+      .setCustomValidity("email Must be Matching.");
+  } else {
+    // input is valid -- reset the error message
+    document
+      .querySelector("#confirmemail")
+      .nextElementSibling.nextElementSibling.classList.remove("hidden");
+    document.querySelector("#confirmemail").setCustomValidity("");
+  }
 
   if (validity) {
     body.style.backgroundColor = "lightgreen";
